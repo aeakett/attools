208a5893b56d3429fe8c921f25aefdcf8286350f
Andrew Eakett <aeakett@gmail.com>
2014-03-19 16:07:49 -0400

    check that a file name is passed

--- get_external_image_references.sh
+++ get_external_image_references.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # initially intended to generate a list of external image references in a warc
 
+if (( $# != 1 )); then
+   echo "Usage: $0 filename.warc.gz"
+	exit 1
+fi
+
 zgrep '<img' $1|sed 's/<img src="/\n/g'|grep '^http'|sed 's/\([^"][^"]*\).*/\1/'|sort|uniq
