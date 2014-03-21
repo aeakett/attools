71d466eeeea7ecde08daab6916a911ba8037ac27
Andrew Eakett <aeakett@gmail.com>
2014-03-21 11:44:01 -0400

    add front end for youtube-dl

---
+++ get_youtube_vid.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if (( $# < 1 )); then
+   echo "Usage: $0 video_url base_file_name"
+        exit 1
+fi
+
+# if we haven't been given a base file name set it to be blank, otherwise, set it appropriatly
+if  [ -z $2 ]
+then
+   BASE_NAME=
+else
+   BASE_NAME="--output $2.%(ext)s"
+fi
+
+YTDL_ARGS="--write-auto-sub --write-description --write-info-json --write-annotations --youtube-include-dash-manifest --all-subs"
+
+./youtube-dl $YTDL_ARGS $BASE_NAME $1
+7z a -t7z $2.subtitles.7z *.srt
+rm *.srt
