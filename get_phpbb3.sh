85d2f4cc20b42ed93bed1d1f03960b47bcf444ed
Andrew Eakett <aeakett@gmail.com>
2014-03-24 19:55:32 +0000

    fix a couple of typos

--- get_phpbb3.sh
+++ get_phpbb3.sh
@@ -18,9 +18,4 @@ then
    echo no path to phpbb3.lua
    exit 1
 fi
-cat ~/.attoolsrc
-echo
-echo ---------------------
-echo asdf --qwer="asdf"$attoolspath
-echo ---------------------
-echo wget-lua --user-agent="Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27" -nv -o $2-$( date +'%Y%m%d' ).log --page-requisites --span-hosts -e "robots=off" --keep-session-cookies --save-cookies=cookies.txt --timeout=10 --tries=3 --waitretry=5 --lua-script=$attollspath/wget-lua-forum-scripts/phpbb3.lua --warc-header="operator: Archive Team" --warc-file=$2-$( date +'%Y%m%d' ) "$1/rss.php" "$1" "$1/index.php"
+wget-lua --user-agent="Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27" -nv -o $2-$( date +'%Y%m%d' ).log --page-requisites --span-hosts -e "robots=off" --keep-session-cookies --save-cookies=cookies.txt --timeout=10 --tries=3 --waitretry=5 --lua-script=$attoolspath/wget-lua-forum-scripts/phpbb3.lua --warc-header="operator: Archive Team" --warc-file=$2-$( date +'%Y%m%d' ) "$1/rss.php" "$1" "$1/index.php"
